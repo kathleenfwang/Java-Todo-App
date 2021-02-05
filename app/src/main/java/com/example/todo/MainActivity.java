a32abb6ce1e762636ef359efc59ed52902d793d0
Kathleen Wang <kathleenchibimaru@gmail.com>
2021-02-04 19:41:42 -0800

    add button edittext rv to main

--- app/src/main/java/com/example/todo/MainActivity.java
+++ app/src/main/java/com/example/todo/MainActivity.java
@@ -1,14 +1,28 @@
 package com.example.todo;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    List<String> items;
+    Button btnAdd;
+    EditText editText;
+    RecyclerView rvItems;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        items = new ArrayList<>();
+        items.add("Wash dishes");
+        items.add("Finish homework");
+        items.add("Play piano");
     }
 }
