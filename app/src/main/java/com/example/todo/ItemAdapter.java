e5b70c91cb07bb2e8545753f715edb1a699589e4
Kathleen Wang <kathleenchibimaru@gmail.com>
2021-02-06 14:08:40 -0800

    add list items constructor to itemsAdapter

--- app/src/main/java/com/example/todo/ItemAdapter.java
+++ app/src/main/java/com/example/todo/ItemAdapter.java
@@ -6,14 +6,21 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.util.List;
+
 public class ItemAdapter extends RecyclerView.Adapter<ItemAdapter.ViewHolder>{
+    // generate constcutor to allow arguments be passed in
+    List<String> items;
+    public ItemAdapter(List<String> items) {
+        this.items = items;
+    }
 
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         return null;
     }
-
+    // use layout inflator to inflate a view
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
 
