95e079e3754cd45bf7bd1c49806028adb3f3bc0a
Kathleen Wang <kathleenchibimaru@gmail.com>
2021-02-05 17:18:30 -0800

    add item adapter

---
+++ app/src/main/java/com/example/todo/ItemAdapter.java
@@ -0,0 +1,16 @@
+package com.example.todo;
+
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class ItemAdapter {
+
+    class ViewHolder extends RecyclerView.ViewHolder{
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
+
+}
